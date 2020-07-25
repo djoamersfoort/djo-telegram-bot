16f5895933061af77601f737b51f6bcb9a95c705
Ronald Moesbergen <github@rmoesbergen.nl>
2020-07-25 09:39:19 +0200

    log error if aanmelden api call fails

--- util/scheduler.py
+++ util/scheduler.py
@@ -26,19 +26,22 @@ class Scheduler(threading.Thread):
             traceback.print_exc()
             return
 
-        if response.ok:
-            slots = response.json()
-            message = ''
-            if slots['friday'] > 0 and slots['saturday'] > 0:
-                message = f"Er zijn op vrijdag nog {slots['friday']} plekken vrij en op zaterdag nog {slots['saturday']}. Vergeet je niet aan te melden!"
-            elif slots['friday'] > 0:
-                message = f"Er zijn op vrijdag nog {slots['friday']} plekken vrij. Vergeet je niet aan te melden!"
-            elif slots['saturday'] > 0:
-                message = f"Er zijn op zaterdag nog {slots['saturday']} plekken vrij. Vergeet je niet aan te melden!"
+        if not response.ok:
+            print(response.content)
+            return
 
-            if message != '':
-                try:
-                    self.bot.send_message(chat_id='@DJOAmersfoort', text=message)
-                except Exception as e:
-                    traceback.print_exc()
-                    pass
+        slots = response.json()
+        message = ''
+        if slots['friday'] > 0 and slots['saturday'] > 0:
+            message = f"Er zijn op vrijdag nog {slots['friday']} plekken vrij en op zaterdag nog {slots['saturday']}. Vergeet je niet aan te melden!"
+        elif slots['friday'] > 0:
+            message = f"Er zijn op vrijdag nog {slots['friday']} plekken vrij. Vergeet je niet aan te melden!"
+        elif slots['saturday'] > 0:
+            message = f"Er zijn op zaterdag nog {slots['saturday']} plekken vrij. Vergeet je niet aan te melden!"
+
+        if message != '':
+            try:
+                self.bot.send_message(chat_id='@DJOAmersfoort', text=message)
+            except Exception as e:
+                traceback.print_exc()
+                pass
